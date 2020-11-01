143061bc1c346231ce2932146d7f0ea64c34ebe1
Yuri Robert Censi <yurirobertcontato@gmail.com>
2020-11-01 14:04:55 -0300

    changes

--- src/components/layout/partials/Logo.js
+++ src/components/layout/partials/Logo.js
@@ -21,10 +21,10 @@ const Logo = ({
       <h1 className="m-0">
         <Link to="/">
           <Image
-            src={require('./../../../assets/images/logo.svg')}
+            src={require('./../../../assets/images/logo_horizontal_exp.png')}
             alt="Open"
-            width={32}
-            height={32} />
+            width={150}
+            height={70} />
         </Link>
       </h1>
     </div>
--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -35,7 +35,7 @@ const Hero = ({
   const closeModal = (e) => {
     e.preventDefault();
     setVideomodalactive(false);
-  }   
+  }
 
   const outerClasses = classNames(
     'hero section center-content',
@@ -61,20 +61,20 @@ const Hero = ({
         <div className={innerClasses}>
           <div className="hero-content">
             <h1 className="mt-0 mb-16 reveal-from-bottom" data-reveal-delay="200">
-              Landing template for <span className="text-color-primary">startups</span>
+              Transformamos sua ideia em <span className="text-color-primary">REALIDADE</span>
             </h1>
             <div className="container-xs">
               <p className="m-0 mb-32 reveal-from-bottom" data-reveal-delay="400">
-                Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.
-                </p>
+                Com nossas ferramentas podemos oferecer a melhor solução para os desafios da sua empresa.
+              </p>
               <div className="reveal-from-bottom" data-reveal-delay="600">
                 <ButtonGroup>
                   <Button tag="a" color="primary" wideMobile href="https://cruip.com/">
-                    Get started
+                    Descubra
                     </Button>
                   <Button tag="a" color="dark" wideMobile href="https://github.com/cruip/open-react-template/">
-                    View on Github
-                    </Button>
+                    Fale conosco
+                  </Button>
                 </ButtonGroup>
               </div>
             </div>
